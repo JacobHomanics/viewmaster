bcaa28cb9e3439f989948ef18a866c27d801cc52
Jacob Homanics <homanicsjake@gmail.com>
2024-10-31 13:21:44 -0500

    base deployments - test

--- packages/foundry/script/DeployYourContract.s.sol
+++ packages/foundry/script/DeployYourContract.s.sol
@@ -12,7 +12,10 @@ contract DeployYourContract is ScaffoldETHDeploy {
     // use `deployer` from `ScaffoldETHDeploy`
     function run() external ScaffoldEthDeployerRunner {
         erc20 mockERC20 = new erc20();
-        Entries entries = new Entries(address(mockERC20), 4);
+        Entries entries = new Entries(
+            0x11dC980faf34A1D082Ae8A6a883db3A950a3c6E8,
+            12
+        );
         // YourContract yourContract = new YourContract(deployer);
         // console.logString(
         //     string.concat(
